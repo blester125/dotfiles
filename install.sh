d33bf8a19d064430d8dcff360fcf3831597cb912
Brian <blester125@gmail.com>
2018-04-18 08:06:55 -0400

    Added common things to bashrc

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ ln -snf $HOME/.shellrc/vim $HOME/.vim
 ln -snf $HOME/.shellrc/vim/vimrc $HOME/.vimrc
 ln -snf $HOME/.shellrc/bash_aliases $HOME/.bash_aliases
 ln -snf $HOME/.shellrc/bash_profile $HOME/.bash_profile
+
+if [ ! -L $HOME/.bashrc ]; then
+    cp $HOME/.bashrc bashrc
+    cat bashrc_add >> bashrc
+    ln -snf $HOME/.shellrc/bashrc $HOME/.bashrc
+fi
