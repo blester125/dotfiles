e9762ccebdd7df3201b8827f37153545d74d8475
Brian <blester125@gmail.com>
2018-04-18 15:41:50 -0400

    updated screen

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -snf $HOME/.shellrc/vim $HOME/.vim
 ln -snf $HOME/.shellrc/vim/vimrc $HOME/.vimrc
 ln -snf $HOME/.shellrc/bash_aliases $HOME/.bash_aliases
 ln -snf $HOME/.shellrc/bash_profile $HOME/.bash_profile
+ln -snf $HOME/.shellrc/screenrc $HOME/.screenrc
 
 if [ ! -L $HOME/.bashrc ]; then
     cp $HOME/.bashrc bashrc
