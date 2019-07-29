e40f27718da94cce208d639c28bd9df9a4cdaaab
Brian <blester125@gmail.com>
2019-07-29 15:11:26 -0400

    dict2object and generator length

---
+++ utils/utils.py
@@ -0,0 +1,20 @@
+from collections import deque, Sequence, Mapping
+from itertools import count
+
+def len_gen(gen):
+    c = count()
+    deque(zip(gen, c), 0)
+    return next(c)
+
+
+class Struct:
+    def __init__(self, d):
+        for k, v in d.items():
+            if isinstance(v, Sequence):
+                setattr(self, k, [Struct(x) if isinstance(x, Mapping) else x for x in v])
+            elif isinstance(v, Mapping):
+                setattr(self, k, Struct(v))
+            else:
+                setattr(self, k, v)
+    def __repr__(self):
+        return repr(self.__dict__)
